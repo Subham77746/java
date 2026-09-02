58e4981bc7d52b4eab66020215a67c8140a87e21
Subham <sachidanandapanigrahy374@gmail.com>
2026-09-02 13:14:34 +0530

    Add files via upload

---
+++ Lecture-1/LINEWISEPRINT.java
@@ -0,0 +1,29 @@
+
+/* 1. (use println to print in diffrent line or use \n) */
+public class LINEWISEPRINT{
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+        System.out.println("Hello world\nhow are you");
+        System.out.print("Hello world");
+    }
+}
+
+/* 2(a). TO PRINT  *
+                   **
+                   ***
+                   *****/
+/* public class  LINEWISEPRINT {
+    public static void main(String args[]) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+} */
+
+/*2(b). OR IT CAN CAN ALSO BE DONE WITH "\n" */
+/* public class  LINEWISEPRINT {
+    public static void main(String args[]) {
+        System.out.print("*\n**\n***\n****");
+    }
+} */
